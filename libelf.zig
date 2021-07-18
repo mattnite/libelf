3ae69b1cb8d06cf7fce833097e0fe5544ed487be
Matthew Knight <mgk1795@gmail.com>
2021-07-17 21:01:58 -0700

    add object interface

--- libelf.zig
+++ libelf.zig
@@ -10,14 +10,23 @@ pub fn includeDir() []const u8 {
     return baseDir() ++ std.fs.path.sep_str ++ "include";
 }
 
+fn applyOpts(lib: *LibExeObjStep, target: std.build.Target, mode: std.builtin.Mode, bundle: bool) void {
+    lib.addIncludeDir(includeDir());
+    lib.setTarget(target);
+    lib.setBuildMode(mode);
+    lib.bundle_compiler_rt = bundle;
+    lib.pie = true;
+    lib.linkLibC();
+}
+
+pub fn addObject(b: *Builder, target: std.build.Target, mode: std.builtin.Mode) *LibExeObjStep {
+    const ret = b.addObject("elf", baseDir() ++ std.fs.path.sep_str ++ "src" ++ std.fs.path.sep_str ++ "main.zig");
+    applyOpts(ret, target, mode, false);
+    return ret;
+}
+
 pub fn addStaticLibrary(b: *Builder, target: std.build.Target, mode: std.builtin.Mode) *LibExeObjStep {
     const ret = b.addStaticLibrary("elf", baseDir() ++ std.fs.path.sep_str ++ "src" ++ std.fs.path.sep_str ++ "main.zig");
-    ret.addIncludeDir(includeDir());
-    ret.setTarget(target);
-    ret.setBuildMode(mode);
-    ret.bundle_compiler_rt = true;
-    ret.pie = true;
-    ret.linkLibC();
-
+    applyOpts(ret, target, mode, true);
     return ret;
 }
