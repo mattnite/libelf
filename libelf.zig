cf2b31dc48bbca77a67a78ea730fdb477d497c3c
Matthew Knight <mattnite@protonmail.com>
2021-07-17 19:51:13 -0700

    improve builder interface (#8)
    * improve builder interface
    
    * refactor build.zig

--- libelf.zig
+++ libelf.zig
@@ -2,31 +2,22 @@ const std = @import("std");
 const Builder = std.build.Builder;
 const LibExeObjStep = std.build.LibExeObjStep;
 
-var libelf: ?*LibExeObjStep = null;
-var include_dir: ?[]const u8 = null;
-
 fn baseDir() []const u8 {
     return comptime std.fs.path.dirname(@src().file) orelse unreachable;
 }
 
 pub fn includeDir() []const u8 {
-    if (include_dir == null) {
-        include_dir = baseDir() ++ std.fs.path.sep_str ++ "include";
-    }
-
-    return include_dir.?;
+    return baseDir() ++ std.fs.path.sep_str ++ "include";
 }
 
-pub fn link(b: *Builder, obj: *LibExeObjStep, target: std.build.Target, mode: std.builtin.Mode) void {
-    if (libelf == null) {
-        libelf = b.addStaticLibrary("elf", baseDir() ++ std.fs.path.sep_str ++ "src" ++ std.fs.path.sep_str ++ "main.zig");
-        libelf.?.addIncludeDir(includeDir());
-        libelf.?.setTarget(target);
-        libelf.?.setBuildMode(mode);
-        libelf.?.bundle_compiler_rt = true;
-        libelf.?.linkLibC();
-    }
+pub fn addStaticLibrary(b: *Builder, target: std.build.Target, mode: std.builtin.Mode) *LibExeObjStep {
+    const ret = b.addStaticLibrary("elf", baseDir() ++ std.fs.path.sep_str ++ "src" ++ std.fs.path.sep_str ++ "main.zig");
+    ret.addIncludeDir(includeDir());
+    ret.setTarget(target);
+    ret.setBuildMode(mode);
+    ret.bundle_compiler_rt = true;
+    ret.pie = true;
+    ret.linkLibC();
 
-    obj.addIncludeDir(includeDir());
-    obj.linkLibrary(libelf.?);
+    return ret;
 }
